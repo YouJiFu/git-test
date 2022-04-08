cad7b86d96e3810421b54a752f70a863d6dd803f
YouJiFu <1812706735@qq.com>
2022-04-08 23:15:23 +0800

    second gitTest!!!

--- src/main/java/com/youjifu/TestGit.java
+++ src/main/java/com/youjifu/TestGit.java
@@ -8,5 +8,6 @@ package com.youjifu;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello Git！！！");
+        System.out.println("second Git !!!");
     }
 }
