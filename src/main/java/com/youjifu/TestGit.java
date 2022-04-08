885d9883e5ac0e4a34d81d8b16082dbd357c60a8
YouJiFu <1812706735@qq.com>
2022-04-08 23:00:51 +0800

    commit gitTest!!!

---
+++ src/main/java/com/youjifu/TestGit.java
@@ -0,0 +1,12 @@
+package com.youjifu;
+
+/**
+ * @author YouJiFu
+ * @version 1.0
+ * @Date 2022/4/8 22:56
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git！！！");
+    }
+}
